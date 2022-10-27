d6d95571c38dfdf74c99afd8e51ac122ea715c93
Yen-Kuang Chen <nf8964p5566@gmail.com>
2022-10-27 08:17:57 +0800

    update: update home page and HomePage class

--- app/src/main/java/welfare/self_care_task_manager/HomePage.kt
+++ app/src/main/java/welfare/self_care_task_manager/HomePage.kt
@@ -1,5 +1,6 @@
 package welfare.self_care_task_manager
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,11 +9,7 @@ import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.navigation.Navigation
 
 /**
  * A simple [Fragment] subclass.
@@ -20,16 +17,12 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomePage : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var activity: Activity
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        activity = requireActivity()
     }
 
     override fun onCreateView(
@@ -42,43 +35,19 @@ class HomePage : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val txt = view.findViewById<TextView>(R.id.testText)
-        var acc: Int = 0
 
         val btnLast = view.findViewById<ImageButton>(R.id.toLastCard)
         val btnNext = view.findViewById<ImageButton>(R.id.toNextCard)
         btnLast.setOnClickListener{
-            acc--
-            txt.text = acc.toString()
+            Toast.makeText(view.context, "param1", Toast.LENGTH_SHORT).show()
         }
         btnNext.setOnClickListener{
-            acc++
-            txt.text = acc.toString()
+            Toast.makeText(view.context, "param2", Toast.LENGTH_SHORT).show()
         }
 
-        var toCassatte = view.findViewById<ImageButton>(R.id.toCassatte)
+        val toCassatte = view.findViewById<ImageButton>(R.id.toCassatte)
         toCassatte.setOnClickListener {
             Toast.makeText(view.context, "YO", Toast.LENGTH_SHORT).show()
         }
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment home_page.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomePage().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
